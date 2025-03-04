07ea21961e74e7319563429fe9019e731d2d14ca
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2025-03-04 23:43:40 +0100

    Add another app called Daily Quote based on vue

---
+++ packages/daily-quote/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Quote />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Quote from './components/Quote.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Quote,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/daily-quote/src/components/Quote.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Quote',
+  setup() {
+    const quotes = [
+      'The best way to predict the future is to invent it.',
+      'Life is 10% what happens to us and 90% how we react to it.',
+      'The only way to do great work is to love what you do.',
+      'Success is not the key to happiness. Happiness is the key to success.',
+      "Your time is limited, don't waste it living someone else's life.",
+    ];
+
+    const quote = ref('');
+
+    const getRandomQuote = () => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      quote.value = quotes[randomIndex];
+    };
+
+    onMounted(() => {
+      getRandomQuote();
+    });
+
+    return {
+      quote,
+    };
+  },
+});
+</script>
+
+<style>
+p {
+  font-size: 1.5em;
+}
+</style>
