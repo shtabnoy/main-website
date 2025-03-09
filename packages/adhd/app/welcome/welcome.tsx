abca6b5407dc9c4a28df28c672794c4da05340c9
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2025-03-09 22:00:11 +0100

    use react-router for adhd app

---
+++ packages/adhd/app/welcome/welcome.tsx
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl text-[#6B8E23]">Welcome to my site</h1>
+    </div>
+  );
+}
--- packages/adhd/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-export default function App() {
-  return (
-    <div className="flex justify-center items-center min-h-screen font-avenir">
-      <h1 className="text-3xl text-emerald-800">Welcome to my site</h1>
-    </div>
-  );
-}
