6c3302d01177da8d0db25a3adcf5bab08c894f95
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2025-03-13 00:41:11 +0100

    update styles

--- packages/adhd/app/welcome/welcome.tsx
+++ packages/adhd/app/welcome/welcome.tsx
@@ -4,8 +4,14 @@ import './welcome.css';
 export function Welcome() {
   const [isClicked, setIsClicked] = useState(false);
 
+  const [showNewText, setShowNewText] = useState(false);
+
   const handleClick = () => {
     setIsClicked(true);
+    setTimeout(() => {
+      setShowNewText(true);
+      document.body.classList.add('new-theme-bg');
+    }, 1000); // Adjust the timeout to match the animation duration
   };
 
   return (
@@ -13,9 +19,15 @@ export function Welcome() {
       className={`flex justify-center items-center min-h-screen`}
       onClick={handleClick}
     >
-      <h1 className="text-4xl text-[#6B8E23]">
+      <h1
+        className={`text-4xl ${showNewText ? 'text-black' : 'text-[#6B8E23]'}`}
+      >
         Welcome to my s
-        <span className={`interactive-dot ${isClicked ? 'expand-circle' : ''}`}>
+        <span
+          className={`interactive-dot ${isClicked ? 'expand-circle' : ''} ${
+            showNewText ? 'hide-dot' : ''
+          }`}
+        >
           i
         </span>
         te
