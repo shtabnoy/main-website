19e6a5656adbe5e7efabc364f8531aeb08d98fd0
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2025-03-13 00:04:59 +0100

    expand the dot

--- packages/adhd/app/welcome/welcome.tsx
+++ packages/adhd/app/welcome/welcome.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import './welcome.css';
+
 export function Welcome() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-4xl text-[#6B8E23]">Welcome to my site</h1>
+    <div
+      className={`flex justify-center items-center min-h-screen`}
+      onClick={handleClick}
+    >
+      <h1 className="text-4xl text-[#6B8E23]">
+        Welcome to my s
+        <span className={`interactive-dot ${isClicked ? 'expand-circle' : ''}`}>
+          i
+        </span>
+        te
+      </h1>
     </div>
   );
 }
